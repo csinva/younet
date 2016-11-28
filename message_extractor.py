fb03f0d777e960473a900070051cd856059be790
Chandan <cs3hq@virginia.edu>
2016-11-27 22:41:04 -0500

    basic message-reading

--- message_extractor.py
+++ message_extractor.py
@@ -1,10 +1,72 @@
 from html.parser import HTMLParser
+from html.entities import name2codepoint
 
 
 def parse_messages(filepath):
     print("reading file...", filepath)
+    with open(filepath) as f:
+        text = f.read()
+    parser = MyHTMLParser()
+    parser.feed(text)
+
+
+class MyHTMLParser(HTMLParser):
+    set_user = False
+    current_user = ""
+    set_message = False
+
+    def handle_starttag(self, tag, attrs):
+        # print("messages:", messages)
+        # print("Start tag:", tag)
+        # for (key, val) in attrs:
+        #     print("     attr:", key, val)
+        if tag == "span" and len(attrs) > 0 and attrs[0][0] == "class" and attrs[0][1] == "user":
+            # print("setting user tag!")
+            self.set_user = True
+        elif tag == "p":
+            self.set_message = True
+            # print("message!")
+
+            # def handle_endtag(self, tag):
+            # print("End tag  :", tag)
+
+    def handle_data(self, data):
+        if self.set_user:
+            self.set_user = False
+            self.current_user = data
+            # print("current user:", data)
+        elif self.set_message:
+            if self.current_user == name:
+                messages.append(data)
+            self.set_message = False
+            # print("Data     :", data)
+
+    # def handle_comment(self, data):
+    #     print("Comment  :", data)
+
+    def handle_entityref(self, name):
+        c = chr(name2codepoint[name])
+        # print("Named ent:", c)
+
+    def handle_charref(self, name):
+        if name.startswith('x'):
+            c = chr(int(name[1:], 16))
+        else:
+            c = chr(int(name))
+            # print("Num ent  :", c)
+
+            # def handle_decl(self, data):
+            #     print("Decl     :", data)
 
 
 if __name__ == "__main__":
-    filepath = "data/chandan/html/messages.htm"
-    parse_messages(filepath)
+    name = "Chandan Singh"
+    # input_file = "data/chandan/messages_readable.html"
+    output_file = "data/chandan/messages_out.html"
+    input_file = "data/chandan/html/messages.htm"
+    messages = []
+    parse_messages(input_file)
+    print('writing file...')
+    with open(output_file, 'w') as f:
+        for line in messages:
+            f.write(line + '\n')
