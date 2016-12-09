df201b48a3d44122187396ef48f57678d493d58a
Chandan <cs3hq@virginia.edu>
2016-12-09 17:25:41 -0500

    sample website

--- web_app/myproject/myapp/views.py
+++ web_app/myproject/myapp/views.py
@@ -10,9 +10,9 @@ from keras.models import load_model
 import numpy as np
 import pickle
 
-char_indices = pickle.load(open(settings.PROJECT_ROOT + '/myapp/char_indices.p', 'rb'))
-indices_char = pickle.load(open(settings.PROJECT_ROOT + '/myapp/indices_char.p', 'rb'))
-model = load_model(settings.PROJECT_ROOT + '/myapp/char_rnn.h5')
+char_indices = pickle.load(open(settings.PROJECT_ROOT + '/myapp/char_indices_3.p', 'rb'))
+indices_char = pickle.load(open(settings.PROJECT_ROOT + '/myapp/indices_char_3.p', 'rb'))
+# model = load_model(settings.PROJECT_ROOT + '/myapp/char_rnn.h5')
 
 
 def sample(preds, temperature=1.0):
@@ -27,23 +27,23 @@ def sample(preds, temperature=1.0):
 def search(request):
     response_length = 50
     prev = ''
-    search_title = request.POST.get('textfield', None)
-    print('searching....', search_title)
-    print("shape char_ind", len(char_indices))
-    print("shape indices_char", len(indices_char))
-    prev = (prev + search_title)[-40:]
-    if len(prev) < 40:
-        prev = ' ' * (40 - len(prev)) + prev
-    response = ''
-    for i in range(response_length):
-        x = np.zeros((1, len(prev), len(char_indices)))
-        print("shape x", x.shape)
-        for t, char in enumerate(prev):
-            x[0, t, char_indices[char]] = 1.
-        preds = model.predict(x, verbose=0)[0]
-        next_char = indices_char[sample(preds, temperature=0.5)]
-        response += next_char
-        prev = prev[1:] + next_char
+    # search_title = request.POST.get('textfield', None)
+    # print('searching....', search_title)
+    # print("shape char_ind", len(char_indices))
+    # print("shape indices_char", len(indices_char))
+    # prev = (prev + search_title)[-40:]
+    # if len(prev) < 40:
+    #     prev = ' ' * (40 - len(prev)) + prev
+    response = 'SAMPLE_RESPONSE'
+    # for i in range(response_length):
+    #     x = np.zeros((1, len(prev), len(char_indices)))
+    # print("shape x", x.shape)
+    # for t, char in enumerate(prev):
+    # x[0, t, char_indices[char]] = 1.
+    # preds = model.predict(x, verbose=0)[0]
+    # next_char = indices_char[sample(preds, temperature=0.5)]
+    # response += next_char
+    # prev = prev[1:] + next_char
     return render_to_response(
         'myapp/list.html',
         {'message': response},
